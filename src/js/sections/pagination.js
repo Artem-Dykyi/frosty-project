1c8afd15552a2f1640b67301662d4f7bbb036399
Smoliarenko Alex <smoliarenko.sasha@gmail.com>
2025-05-30 14:27:25 +0300

    made numbers when use pagination

--- src/js/sections/pagination.js
+++ src/js/sections/pagination.js
@@ -3,23 +3,47 @@ const pagRightTwo = document.querySelector('#pagination__right--two');
 const pagLeft = document.querySelector('#pagination__left');
 const pagLeftTwo = document.querySelector('#pagination__left--two');
 const ProductList = document.querySelector('.products__list');
+const pagination = document.querySelector(".pagination__centers");
 
 const api = "https://food-boutique.b.goit.study/api/products";
-let pages = 0;
-fetch(api)
-  .then(response => response.json())
-  .then(data => {
-    // console.log(data);
-    pages = Math.ceil(data.length / 6);
-  })
 let currentPage = 1;
+let totalPages = 1;
+
+pagRight.addEventListener('click', () => {
+  if (currentPage < totalPages) {
+    currentPage++;
+    showPage(api, currentPage);
+  }
+});
+pagLeft.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    showPage(api, currentPage);
+  }
+});
+pagRightTwo.addEventListener('click', () => {
+  if (currentPage < totalPages) {
+    currentPage = totalPages;
+    showPage(api, currentPage);
+  }
+});
+pagLeftTwo.addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage = 1;
+    showPage(api, currentPage);
+  }
+});
 
 async function showPage(api, currentPage) {
   const response = await fetch(`${api}?page=${currentPage}&limit=6`);
   const data = await response.json();
-  console.log(data.results);
+  console.log(data); 
+  totalPages = data.totalPages;
   let markup = '';
-  data.results.forEach(item => {
+
+  const products = data.results || data;
+
+  products.forEach(item => {
     markup += `
     <li class="products__item">
         <div class="products__content">
@@ -56,35 +80,73 @@ async function showPage(api, currentPage) {
         </div>
       </li>`;
   });
+
   if (ProductList) {
     ProductList.innerHTML = markup;
   }
-  pagRight.addEventListener('click', () => {
-    if (currentPage < data.totalPages) {
-      currentPage++;
-      showPage(api, currentPage);
-    }
-  });
-  pagLeft.addEventListener('click', () => {
-    if (currentPage > 1) {
-      currentPage--;
-      showPage(api, currentPage);
-    }
-  });
-  pagRightTwo.addEventListener('click', () => {
-    if (currentPage < data.totalPages) {
-      currentPage = data.totalPages;
-      showPage(api, currentPage);
-    }
-  });
-  pagLeftTwo.addEventListener('click', () => {
-    if (currentPage > 1) {
-      currentPage = 1;
-      showPage(api, currentPage);
-    }
-  });
+  updatePaginationButtons();
+  createPagination(api, currentPage);
 }
 
-showPage(api, currentPage)
+function updatePaginationButtons() {
+  if (currentPage === 1) {
+    pagLeft.classList.add('pagination__arrow--disabled');
+    pagLeftTwo.classList.add('pagination__arrow--disabled');
+  } else {
+    pagLeft.classList.remove('pagination__arrow--disabled');
+    pagLeftTwo.classList.remove('pagination__arrow--disabled');
+  }
+  if (currentPage === totalPages) {
+    pagRight.classList.add('pagination__arrow--disabled');
+    pagRightTwo.classList.add('pagination__arrow--disabled');
+  } else {
+    pagRight.classList.remove('pagination__arrow--disabled');
+    pagRightTwo.classList.remove('pagination__arrow--disabled');
+  }
+}
 
+function createPagination(api, currentPage) {
+  let markup = '';
+  if (totalPages <= 4) {
+    for (let i = 1; i <= totalPages; i++) {
+      markup += `<li class="pagination__center${i === currentPage ? ' pagination__center--active' : ''}">
+        <p class="pagination__number${i === currentPage ? ' pagination__number--active' : ''}">${i}</p>
+      </li>`;
+    }
+  } else {
+    if (currentPage <= 3) {
+      for (let i = 1; i <= 4; i++) {
+        markup += `<li class="pagination__center${i === currentPage ? ' pagination__center--active' : ''}">
+          <p class="pagination__number${i === currentPage ? ' pagination__number--active' : ''}">${i}</p>
+        </li>`;
+      }
+      markup += `<li class="pagination__center">
+        <p class="pagination__number">...</p>
+      </li>`;
+    } else if (currentPage >= totalPages - 2) {
+      markup += `<li class="pagination__center">
+        <p class="pagination__number">...</p>
+      </li>`;
+      for (let i = totalPages - 3; i <= totalPages; i++) {
+        markup += `<li class="pagination__center${i === currentPage ? ' pagination__center--active' : ''}">
+          <p class="pagination__number${i === currentPage ? ' pagination__number--active' : ''}">${i}</p>
+        </li>`;
+      }
+    } else {
+      markup += `<li class="pagination__center">
+        <p class="pagination__number">...</p>
+      </li>`;
+      for (let i = currentPage - 1; i <= currentPage + 2; i++) {
+        markup += `<li class="pagination__center${i === currentPage ? ' pagination__center--active' : ''}">
+          <p class="pagination__number${i === currentPage ? ' pagination__number--active' : ''}">${i}</p>
+        </li>`;
+      }
+      markup += `<li class="pagination__center">
+        <p class="pagination__number">...</p>
+      </li>`;
+    }
+  }
+  pagination.innerHTML = markup;
+}
 
+showPage(api, currentPage);
