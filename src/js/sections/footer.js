4d1d3ed8a2ed53ce140927d3952448859a9ca68b
der1x <130235333+Mark4353@users.noreply.github.com>
2025-06-06 10:33:07 +0300

    footer-js

---
+++ src/js/sections/footer.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("subscribeForm");
+  const emailInput = document.getElementById("footerSubscribeInput");
+  const successModal = document.querySelector(".subscribe_modal-backdrop");
+  const errorModal = document.querySelector(".already-sub__modal");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    if (!emailInput.checkValidity()) {
+      emailInput.reportValidity();
+      return;
+    }
+
+    const email = emailInput.value.trim();
+
+    try {
+      const response = await fetch(
+        "https://food-boutique.b.goit.study/api/subscription",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email }),
+        }
+      );
+
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log("Success:", result);
+        emailInput.value = "";
+        successModal.classList.remove("hidden");
+      } else {
+        console.log("Error response:", result);
+        errorModal.classList.remove("hidden");
+      }
+    } catch (error) {
+      console.log("Network error:", error);
+      errorModal.classList.remove("hidden");
+    }
+  });
+});
